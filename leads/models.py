357ffb1b163a15ce4e1eaea1807169c8fbf42f21
Akonya John <johnakon>
2021-04-07 23:51:46 +0300

    created more models

--- leads/models.py
+++ leads/models.py
@@ -1,8 +1,35 @@
 from django.db import models
+# from django.contrib.auth import get_user_model
 
-# Create your models here.
+# User = get_user_model()
+
+# import abstract user
+from django.contrib.auth.models import AbstractUser
+
+# custom User model
+# inherit from AbstractUser
+class User(AbstractUser):
+    pass
 
 class Lead(models.Model):
+
     first_name = models.CharField(max_length=20)
     last_name = models.CharField(max_length=20)
     age = models.IntegerField(default=0)
+
+    # every lead will have its own agaent
+    agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
+    # deleting an agent deletes a lead
+
+
+# an agent to manage a lead
+class Agent(models.Model):
+    # if a user is deleted, their agent is also deleted
+    # one agent for every one user-> OneToOneField --linked to the User model
+    user = models.OneToOneField(User, on_delete=models.CASCADE)
+  
+
+
+
+
+

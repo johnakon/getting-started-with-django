e0a7e36d0fdf001ba7bb8c413a70f4e1e1e2cb62
Akonya John <johnakon>
2021-04-08 00:36:59 +0300

    inteactions with the model manager

--- leads/models.py
+++ leads/models.py
@@ -21,13 +21,19 @@ class Lead(models.Model):
     agent = models.ForeignKey("Agent", on_delete=models.CASCADE)
     # deleting an agent deletes a lead
 
+    def __str__(self):
+        return f"{self.first_name} {self.last_name}"
+
 
 # an agent to manage a lead
 class Agent(models.Model):
     # if a user is deleted, their agent is also deleted
     # one agent for every one user-> OneToOneField --linked to the User model
     user = models.OneToOneField(User, on_delete=models.CASCADE)
-  
+    
+    # return string definition
+    def __str__(self):
+        return self.user.email
 
 
 

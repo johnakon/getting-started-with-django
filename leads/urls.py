58577f1bea6401ec3f50d79184fbbfa1f9ca5a80
Akonya John <johnakon>
2021-04-11 00:04:41 +0300

    adding lead details

--- leads/urls.py
+++ leads/urls.py
@@ -1,9 +1,11 @@
 from django.urls import path
-from .views import  lead_list
+from .views import  lead_list, lead_detail
 
 app_name = "leads"
 
 # create a urlpattern list
 urlpatterns =[
-    path('', lead_list)
+    path('', lead_list),
+    # use pk to uniquely identify a lead
+    path('<pk>/', lead_detail)
 ]
--- leads/views.py
+++ leads/views.py
@@ -5,6 +5,8 @@ from .models import Lead
 
 
 # function based views
+
+# lead list
 def lead_list(request):
 
     # fetch all leads and store in a variable leads
@@ -14,3 +16,15 @@ def lead_list(request):
         "leads" : leads
     }
     return render(request, "leads/lead_list.html", context)
+
+
+# leads details
+# a primary key is also passed
+def lead_detail(request, pk):
+    print(pk)
+    # use pk to retrieve specific row using object manager
+    lead = Lead.objects.get(id=pk)
+    print(lead)
+    return HttpResponse("Here is the leads view")
+
+    

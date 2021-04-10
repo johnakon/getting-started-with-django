971ea5467ae490d2189f9bb5af6397d975ea0e96
Akonya John <johnakon>
2021-04-11 02:58:25 +0300

    updating a lead instance

--- leads/views.py
+++ leads/views.py
@@ -51,14 +51,66 @@ def lead_create(request):
     return render(request, "leads/lead_create.html", context)
 
 
-# updating the lead
+
+# updating using LeadModelForm
 def lead_update(request, pk):
-    lead = Lead.objects.get(id=pk)
+    lead = Lead.objects.get(id=pk)  
+    # update specific instance of Model form
+
+    form = LeadModelForm(instance=lead)
+     
+    if request.method == "POST":
+        # pre-populates the form with existing data
+        form = LeadModelForm(request.POST, instance=lead)
+        if form.is_valid():
+            print("form is valid")
+            form.save()
+
+            return redirect("/leads")
+
+
     context = {
+        "form" : form,
         "lead" : lead
     }
+    return render(request, "leads/lead_create.html", context)
 
-    return render(request, "leads/lead_update.html", context)
+# updating the lead
+# def lead_update(request, pk):
+#     lead = Lead.objects.get(id=pk)
+
+#     form = LeadForm() 
+
+#     if request.method == "POST":
+#         print("Receiving a post request")
+#         form = LeadForm(request.POST)
+#         # check if form is valid
+#         if form.is_valid():
+#             print("form is valid")
+#             print(form.cleaned_data)   
+#             first_name = form.cleaned_data['first_name']
+#             last_name = form.cleaned_data['last_name']
+#             age = form.cleaned_data['age']
+#             # agent = Agent.objects.first() 
+#             # updating each value on this model
+#             lead.first_name = first_name
+#             lead.last_name = last_name
+#             lead.age = age
+
+#             # commit values to the db
+#             lead.save()
+
+#             print("Lead has been created")
+#             # redirect
+#             return redirect("/leads")
+
+
+#     context = {
+#         "form" : form,
+#         "lead" : lead
+#     }
+
+#     return render(request, "leads/lead_update.html", context)
 
 
 

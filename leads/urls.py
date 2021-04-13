5791b06f8bd73b32361b4c45c25e05bcff4cf632
Akonya John <johnakon>
2021-04-13 21:06:17 +0300

    working with class based views

--- leads/urls.py
+++ leads/urls.py
@@ -1,7 +1,7 @@
 from django.urls import path
 from .views import  (
     lead_list, lead_detail, lead_create, lead_update, lead_delete, 
-    LeadListView
+    LeadListView, LeadDetailView, LeadCreateView, LeadUpdateView, LeadDeleteView
 )
 
 app_name = "leads"
@@ -10,10 +10,13 @@ app_name = "leads"
 urlpatterns =[
     # path('', lead_list, name='lead-list'), 
     path('', LeadListView.as_view(), name='lead-list'),
-    # path('create/', lead_create),
-    # use pk to uniquely identify a lead
-    path('<int:pk>/', lead_detail, name='lead-detail'),
-    path('<int:pk>/update/', lead_update, name='lead-update'),
-    path('<int:pk>/delete/', lead_delete, name='lead-delete'),
-    path('create/', lead_create, name='lead-create'),
+    # path('<int:pk>/', lead_detail, name='lead-detail'),
+    path('<int:pk>/', LeadDetailView.as_view(), name='lead-detail'),
+    # path('<int:pk>/update/', lead_update, name='lead-update'),
+    path('<int:pk>/update/', LeadUpdateView.as_view(), name='lead-update'),
+    # path('<int:pk>/delete/', lead_delete, name='lead-delete'),
+    path('<int:pk>/delete/', LeadDeleteView.as_view(), name='lead-delete'),
+    # path('create/', lead_create, name='lead-create'),
+    path('create/', LeadCreateView.as_view(), name='lead-create'),
+
 ]
--- leads/views.py
+++ leads/views.py
@@ -1,6 +1,6 @@
-from django.shortcuts import render, redirect
+from django.shortcuts import render, redirect, reverse
 from django.http import HttpResponse
-from django.views.generic import TemplateView, ListView
+from django.views.generic import TemplateView, ListView, DetailView, CreateView, UpdateView, DeleteView
 from .models import Lead, Agent
 from .forms import LeadForm, LeadModelForm
 
@@ -22,9 +22,8 @@ def landing_page(request):
 class LeadListView(ListView):
     template_name = "leads/lead_list.html"
     queryset = Lead.objects.all()
-    print(queryset)
-    # "object_list" is the default for our queryset
-
+    context_object_name = "leads"
+   
 
 # lead list
 def lead_list(request):
@@ -32,10 +31,15 @@ def lead_list(request):
 
     context={
         "leads" : leads
-        # "queryset":leads
-    }
+     }
     return render(request, "leads/lead_list.html", context)
 
+
+# Detail view as a class using DetailView
+class LeadDetailView(DetailView):
+    template_name = "leads/lead_detail.html"
+    queryset = Lead.objects.all()
+    context_object_name = "lead"
 
 # leads details
 def lead_detail(request, pk):
@@ -44,6 +48,17 @@ def lead_detail(request, pk):
         "lead" : lead
     }
     return render(request, "leads/lead_detail.html", context)
+
+
+# Create view as a class using CreateView
+class LeadCreateView(CreateView):
+    template_name = "leads/lead_create.html"
+    form_class = LeadModelForm
+
+    # specific method when form is saved successfully
+    def get_success_url(self):  
+        # return "/leads"  
+        return reverse("leads:lead-list")
 
 
 # creating a lead from 
@@ -64,6 +79,18 @@ def lead_create(request):
     return render(request, "leads/lead_create.html", context)
 
 
+
+
+# update view as a class using UpdateView
+class LeadUpdateView(UpdateView):
+    template_name = "leads/lead_update.html"
+    queryset = Lead.objects.all()
+    form_class = LeadModelForm
+
+    # specific method when form is saved successfully
+    def get_success_url(self):  
+        # return "/leads"  
+        return reverse("leads:lead-list")
 
 # updating using LeadModelForm
 def lead_update(request, pk):
@@ -90,6 +117,16 @@ def lead_update(request, pk):
 
 
 
+# delete view as a class using DEleteView
+class LeadDeleteView(DeleteView):
+    template_name = "leads/lead_delete.html"
+    queryset = Lead.objects.all() 
+
+    # specific method when form is saved successfully
+    def get_success_url(self):  
+        # return "/leads"  
+        return reverse("leads:lead-list")
+
 # deleting a lead
 def lead_delete(request, pk):
     lead = Lead.objects.get(id=pk)  
@@ -98,78 +135,3 @@ def lead_delete(request, pk):
     form = LeadModelForm(instance=lead)
 
     return redirect("/leads")
-
-
-# updating the lead
-# def lead_update(request, pk):
-#     lead = Lead.objects.get(id=pk)
-
-#     form = LeadForm() 
-
-#     if request.method == "POST":
-#         print("Receiving a post request")
-#         form = LeadForm(request.POST)
-#         # check if form is valid
-#         if form.is_valid():
-#             print("form is valid")
-#             print(form.cleaned_data)   
-#             first_name = form.cleaned_data['first_name']
-#             last_name = form.cleaned_data['last_name']
-#             age = form.cleaned_data['age']
-#             # agent = Agent.objects.first() 
-#             # updating each value on this model
-#             lead.first_name = first_name
-#             lead.last_name = last_name
-#             lead.age = age
-
-#             # commit values to the db
-#             lead.save()
-
-#             print("Lead has been created")
-#             # redirect
-#             return redirect("/leads")
-
-
-#     context = {
-#         "form" : form,
-#         "lead" : lead
-#     }
-
-#     return render(request, "leads/lead_update.html", context)
-
-
-
-
-# for reference
-# creating a lead from 
-# def lead_create(request):
-#     form = LeadForm()  # empty form instantiation
-#     # print(request.POST)
-
-#     # check if method is POST 
-#     if request.method == "POST":
-#         print("Receiving a post request")
-#         form = LeadForm(request.POST)
-#         # check if form is valid
-#         if form.is_valid():
-#             print("form is valid")
-#             print(form.cleaned_data)   # clean data dispaly 
-#             first_name = form.cleaned_data['first_name']
-#             last_name = form.cleaned_data['last_name']
-#             age = form.cleaned_data['age']
-#             agent = Agent.objects.first()  # first row in table  -> similar to .get
-#             Lead.objects.create(
-#                 first_name = first_name,
-#                 last_name = last_name,
-#                 age = age,
-#                 agent = agent
-#             )
-#             print("Lead has been created")
-#             # redirect
-#             return redirect("/leads")
-
-
-#     context = {
-#         "form" : form
-#     }
-#     return render(request, "leads/lead_create.html", context)
